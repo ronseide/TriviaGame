acbec94cf61a40c71e967ba51404adfbf8c6fde7
ronseide <ron.seide@gmail.com>
2019-02-28 18:03:41 -0500

    All but the timers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,81 +1,117 @@
 $(document).ready(function () {
-    //What I want to do here is construct an array called quotes that contains quote objects q1, q2...q10 and then be able to iterate through the array, displaying each quote object in order upon a state change to var isSatchel.
-    // var quotes = [q1, q2,];
-    // var q1 = "q1 object as below";
-    // var q2 = "q2 object as below";
-    // console.log (quotes[0]);
     var qTimer = 0;
     var qOver = false;
-    var aTime = 0;
-    var response ;
+    var aTimer = 0;
+    // var aTimer = setInterval(timerForA, 1000);
+    var userScore = 0;
     var currentQuestion = 0;
     var questions = [{
         qHeadline: "Question One",
         quote: "<blockquote>&quot;Don't look back, somebody might be gaining on you.&quot;</blockquote>",
-        isSatchel : true,
-        gotRight: "Yup, Satchel said that.",
-        gotWrong: "Nope, Satchel didn't say that.",
+        isSatchel: true,
+        gotRight: "Q1 got right",
+        gotWrong: "Q1 got wrong",
         goNoTime: "Time's up.",
         image: "assets/images/satchel_1.php"
     },
-        {
-        qHeadline: "Question One",
-        quote: "<blockquote>&quot;Don't look back, somebody might be gaining on you.&quot;</blockquote>",
-        isSatchel : true,
-        gotRight: "Yup, Satchel said that.",
-        gotWrong: "Nope, Satchel didn't say that.",
+    {
+        qHeadline: "Question Two",
+        quote: "<blockquote>&quot;The heart wants what it wants.&quot;</blockquote>",
+        isSatchel: false,
+        gotRight: "Q2 got right",
+        gotWrong: "Q2 got wrong",
+        goNoTime: "Time's up.",
+        image: "assets/images/emilyD.jpg"
+    },
+    {
+        qHeadline: "Question Three",
+        quote: "&quot;The business of America is business.&quot;",
+        isSatchel: false,
+        gotRight: "Q3 got right",
+        gotWrong: "Q3 got wrong",
+        goNoTime: "Time's up.",
+        image: "assets/images/calvinC.jpg"
+    },
+    {
+        qHeadline: "Question Four",
+        quote: "<blockquote>&quot;Avoid fried foods.&quot;</blockquote>",
+        isSatchel: true,
+        gotRight: "Q4 got right",
+        gotWrong: "Q4 got wrong",
         goNoTime: "Time's up.",
         image: "assets/images/satchel_1.php"
     },
-];
-
-    $("#startGame").on("click", function() {
-        // $("#qHeadline").addClass("invisible");
-        displayQuestion ();
+    ];
+    $("#startGame").on("click", function () {
+        displayQuestion()
     });
-    $("#qHeadline").html(questions[currentQuestion].qHeadline);
-    $("#qGraph").html(questions[currentQuestion].quote);
-    $("#qButtons").html(
-        `<ul>
+    $("#qButtons").on("click", "#isSatchel", function () {
+        if (questions[currentQuestion].isSatchel === true) {
+            $("#aGraph").html(questions[currentQuestion].gotRight);
+            userScore++;
+            $("#aTimer").html("Time Remaining " + aTimer + " seconds");
+            $("#imgGraph").attr("src", questions[currentQuestion].image);
+            if (userScore < 3) {
+                $("#userScore").html("Your score is " + userScore);
+            }
+            else
+                $("#userScore").html("Game Over, you got " + userScore + " right");
+            currentQuestion++;
+            //A call to a timer function for aTimer would go here and would call function displayQuestion when aTimer === 0
+        }
+        else
+            if (questions[currentQuestion].isSatchel !== true) {
+                $("#aGraph").html(questions[currentQuestion].gotWrong);
+                $("#aTimer").html("Time Remaining " + aTimer + " seconds");
+                $("#imgGraph").attr("src", questions[currentQuestion].image);
+                currentQuestion++;
+                //A call to a timer function for aTimer would go here and would call function displayQuestion when aTimer === 0
+
+            }
+        //A timer for qTimer would go here as the third option to end the question.
+
+    });
+    $("#qButtons").on("click", "#notSatchel", function () {
+        if (questions[currentQuestion].isSatchel === true) {
+            $("#aGraph").html(questions[currentQuestion].gotWrong);
+            $("#aTimer").html("Time Remaining " + aTimer + " seconds");
+            $("#imgGraph").attr("src", questions[currentQuestion].image);
+            currentQuestion++;
+            //A call to a timer function for aTimer would go here and would call function displayQuestion when aTimer === 0
+
+        }
+        else
+            if (questions[currentQuestion].isSatchel !== true) {
+                $("#aGraph").html(questions[currentQuestion].gotRight);
+                userScore++;
+                $("#aTimer").html("Time Remaining " + aTimer + " seconds");
+                $("#imgGraph").attr("src", questions[currentQuestion].image);
+                //A call to a timer function for aTimer would go here and would call function displayQuestion when aTimer === 0
+
+                if (userScore < 3) {
+                    $("#userScore").html("Your score is " + userScore);
+                }
+                else
+                    $("#userScore").html("Game Over, you got " + userScore + " right");
+                currentQuestion++;
+            }
+        //A timer for qTimer would go here as the third option to end the question.
+    });
+    $("#newQuestion").on("click", function () {
+        displayQuestion()
+    });
+    function displayQuestion() {
+        $("#qHeadline").html(questions[currentQuestion].qHeadline);
+        $("#qGraph").html(questions[currentQuestion].quote);
+        $("#qButtons").html(
+            `<ul>
             <li><label><input type="radio" name="optradio" id="isSatchel">&nbsp;&nbsp;That sounds like Satchel</label></li>
             <li><label><input type="radio" name="optradio" id="notSatchel">&nbsp;&nbsp;That doesn't sound like Satchel</label></li>
         </ul>`
         );
-    $("#isSatchel").on("click", function () {
-        if (questions[currentQuestion].isSatchel === true) {
-            console.log(questions[currentQuestion].gotRight);
-            currentQuestion ++;
-            displayQuestion ();
-        }
-        else
-            console.log(q1.gotWrong);
-        // displayAnswer ();
-    });
-    function displayQuestion () {
-        console.log("hello displayQuestion")
-        $()
+        $("#aGraph").html("");
+        $("#aTimer").html("");
+        $("#imgGraph").attr("src", "assets/images/white.jpg");
+        $("#qTimer").html("Time Remaining " + qTimer + " seconds");
     };
-    $("#qTimer").html("Time Remaining " + qTimer + " seconds");
-    //This is just a mess.
-    // function displayAnswer () {
-    //     if (gotRight === true) {
-    //     $("aGraph").html("Yes, Satchel said that");
-    //     }
-    //     else
-    //     if (gotWrong === true) {
-    //         $("aGraph").html("No, Satchel didn't say that");
-    //     }
-    // };
-    
-    
-
-    $("#imgGraph").attr("src", questions[currentQuestion].image);
-
-
-
-
-
-
-
-
 });
